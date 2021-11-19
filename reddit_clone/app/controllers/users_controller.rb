c3b0952d34c7a0ee29cbc62a09cbc537b5c594ea
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-19 14:18:37 -0600

    connected create user_controller method with new view

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
         render :index
     end
 
-    def create
-        
+    def new
+        #@user = User.find_by(username: params[:username])
+        @user = User.new
+        render :new
     end
 
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            #redirect_to new_user_url
+            redirect_to users_url
+        end
+    end
 
     def user_params
         params.require(:user).permit(:username, :password)
